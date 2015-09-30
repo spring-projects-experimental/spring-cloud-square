f76364af7b216b07c621ab0bfaf7c87121de70a7
Spencer Gibb <spencer@gibb.us>
2015-09-29 20:39:06 -0600

    comments and refactor test

--- src/test/java/org/springframework/cloud/square/okhttp/OkHttpRibbonInterceptorTests.java
+++ src/test/java/org/springframework/cloud/square/okhttp/OkHttpRibbonInterceptorTests.java
@@ -4,9 +4,6 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.SneakyThrows;
 
 import org.junit.Test;
@@ -29,6 +26,7 @@ import retrofit.JacksonConverterFactory;
 import retrofit.Retrofit;
 import retrofit.http.GET;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netflix.loadbalancer.Server;
 import com.netflix.loadbalancer.ServerList;
 import com.squareup.okhttp.OkHttpClient;
@@ -40,9 +38,36 @@ import com.squareup.okhttp.Response;
 @WebIntegrationTest(randomPort = true)
 public class OkHttpRibbonInterceptorTests {
 
+	//The serviceId that ribbon will resolve to host:port
+	public static final String SERVICE_ID = "testapp";
+
+	//interface that retrofit will create an implementation for
+	public interface TestAppClient {
+		@GET("/hello")
+		Call<Hello> hello();
+	}
+
+	//our data object
+	public static class Hello {
+		private String value;
+
+		//for serialization
+		Hello(){}
+
+		public Hello(String value) {
+			this.value = value;
+		}
+
+		public String getValue() {
+			return value;
+		}
+	}
+
+	//is configured with OkHttpRibbonInterceptor
 	@Autowired
 	OkHttpClient httpClient;
 
+	//Our retrofit defined client
 	@Autowired
 	TestAppClient testAppClient;
 
@@ -50,11 +75,14 @@ public class OkHttpRibbonInterceptorTests {
 	@SneakyThrows
 	public void httpClientWorks() {
 		Request request = new Request.Builder()
-				.url("http://testapp/hello")
+				// here you use a service id, or virtual hostname
+				// rather than an actual host:port, ribbon will
+				// resolve it
+				.url("http://" + SERVICE_ID + "/hello")
 				.build();
 		Response response = httpClient.newCall(request).execute();
-		String hello = response.body().string();
-		assertThat("response was wrong", hello, is(equalTo("hello okhttp")));
+		Hello hello = new ObjectMapper().readValue(response.body().byteStream(), Hello.class);
+		assertThat("response was wrong", hello.getValue(), is(equalTo("hello okhttp")));
 	}
 
 	@Test
@@ -68,22 +96,23 @@ public class OkHttpRibbonInterceptorTests {
 	@Configuration
 	@EnableAutoConfiguration
 	@RestController
-	@RibbonClient(name = "testapp", configuration = TestAppConfig.class)
+	// since this is a test we're giving a very specific ribbon configuration
+	// normally this would automatically come from a service discovery
+	// implementation like eureka from spring-cloud-netflix or spring-cloud-consul
+	@RibbonClient(name = SERVICE_ID, configuration = TestAppConfig.class)
 	public static class TestApp {
 
 		@RequestMapping("/hello")
-		public String hello() {
-			return "hello okhttp";
-		}
-
-		@RequestMapping("/hello2")
-		public Hello hello2() {
+		public Hello hello() {
 			return new Hello("hello okhttp");
 		}
 
 		@Bean
 		public TestAppClient testAppClient(OkHttpClient httpClient) {
 			Retrofit retrofit = new Retrofit.Builder()
+					// here you use a service id, or virtual hostname
+					// rather than an actual host:port, ribbon will
+					// resolve it
 					.baseUrl("http://testapp")
 					.client(httpClient)
 					.addConverterFactory(JacksonConverterFactory.create())
@@ -92,20 +121,9 @@ public class OkHttpRibbonInterceptorTests {
 		}
 	}
 
-	public static interface TestAppClient {
-		@GET("/hello2")
-		Call<Hello> hello();
-	}
-
-	@Data
-	@AllArgsConstructor
-	@NoArgsConstructor
-	public static class Hello {
-		private String value;
-	}
-
+	// ribbon configuration that resolves SERVICE_ID to localhost and
+	// the resolved random port
 	public static class TestAppConfig {
-
 		@Value("${local.server.port}")
 		private int port = 0;
 
